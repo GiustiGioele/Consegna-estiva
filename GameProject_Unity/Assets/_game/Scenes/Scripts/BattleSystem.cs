5d087858e3732596a32ffc0187867548f64c2292
Gioele Giusti <gioele.pg@gmail.com>
2022-09-19 17:46:45 +0100

    Attack button and enemy's battle condition

--- GameProject_Unity/Assets/_game/Scenes/Scripts/BattleSystem.cs
+++ GameProject_Unity/Assets/_game/Scenes/Scripts/BattleSystem.cs
@@ -8,4 +8,14 @@ public class BattleSystem : MonoBehaviour
     public int damage;
     public int maxHP;
     public int currentHP;
+
+    public bool TakeDamage (int damage)
+    {
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
 }
--- GameProject_Unity/Assets/_game/Scenes/Scripts/BattleSystem2.cs
+++ GameProject_Unity/Assets/_game/Scenes/Scripts/BattleSystem2.cs
@@ -8,8 +8,8 @@ public class BattleSystem2 : MonoBehaviour
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
 
-    public Transform playerBattleStation;
-    public Transform enemyBattleStation;
+    public Transform[] playerBattleStation;
+    public Transform[] enemyBattleStation;
 
     BattleSystem playerBattle;
     BattleSystem enemyBattle;
@@ -23,18 +23,48 @@ public class BattleSystem2 : MonoBehaviour
     private void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-   void SetupBattle ()
+   IEnumerator SetupBattle ()
     {
-        GameObject playerGo = Instantiate(playerPrefab, playerBattleStation);
-        playerBattle = playerGo.GetComponent<BattleSystem>();
+        for (int i = 0; i < playerBattleStation.Length; i++)
+        {
+            GameObject playerGo = Instantiate(playerPrefab, playerBattleStation[i]);
+            playerBattle = playerGo.GetComponent<BattleSystem>();
 
-       GameObject enemyGo =  Instantiate(enemyPrefab, enemyBattleStation);
-        enemyBattle = enemyGo.GetComponent<BattleSystem>();
-
+            GameObject enemyGo = Instantiate(enemyPrefab, enemyBattleStation[i]);
+            enemyBattle = enemyGo.GetComponent<BattleSystem>();
+        }
         playerHP.SetHp(playerBattle);
         enemyHP.SetHp(enemyBattle);
+
+        yield return new WaitForSeconds(1f);
+
+        state = BattleState.PLAYERTURN;
+        
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyBattle.TakeDamage(playerBattle.damage);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            //End the battle
+        }else
+        {
+            //Enemy turn 23.01
+        }
+
+        
+    }
+
+   public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
     }
 }
